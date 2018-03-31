bac6ea8343a091be1a3af09da858b06808ee5b7a
NirupanM <nirupanm007@gmail.com>
2018-03-31 13:25:10 +0530

    first

---
+++ reactjs/sample/app.js
@@ -0,0 +1,18 @@
+
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+      dssf
+      </div>
+    );
+  }
+}
+
+const render = () => ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
+
+render();
